4798f34bae7b2275604242e96a7a0322cd07d594
andoniVT <andoni.valverde@ucsp.edu.pe>
2015-05-20 11:33:52 -0500

    manager

--- Project/main.py
+++ Project/main.py
@@ -11,9 +11,11 @@ from Settings import corpus_test1 as test1 , corpus_test2 as test2 , corpus_test
 class Manager(object):
     
     def __init__(self):
-        self.__train_data = []
+        self.__train_data = self.readTrainData()
     
-    def getTrainData(self):
+    def readTrainData(self):
+        comments = []
+        labels = []
         reader = Reader(train1, 1)
         corpus = reader.get_comments()
         for i in corpus:
@@ -21,13 +23,31 @@ class Manager(object):
                 proc = TextCleaner(i[0])
                 texto_procesado = proc.get_processed_comment()
                 if texto_procesado!= "None!":
-                    print texto_procesado + " - " + i[1]   
+                    comments.append(texto_procesado)
+                    if i[1] == "P+":                                            
+                        labels.append("P")
+                    elif i[1] == "N+":                                                                    
+                        labels.append("N")                            
+                    else:                                                 
+                        labels.append(i[1])
+        return [comments , labels]   
+    
+    def getData(self):
+        return self.__train_data
 
 
 if __name__ == '__main__':
     
-    obj = Manager()
-    obj.getTrainData()    
+    obj = Manager()    
+    corpus = obj.getData()
+    comments = corpus[0]
+    labels = corpus[1]
+    
+    print len(comments)
+    print len(labels)
+    
+    
+        
     '''
     for i in corpus:         
         if i is not None:
